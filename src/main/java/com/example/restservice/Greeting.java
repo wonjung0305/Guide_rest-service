797db0166fa4afba92eb70f9a249bb4296d5e9b9
wonjung <wonjung1919@gmail.com>
2026-07-21 14:40:45 +0900

    feat: add postMapping

--- src/main/java/com/example/restservice/Greeting.java
+++ src/main/java/com/example/restservice/Greeting.java
@@ -1,3 +1,9 @@
 package com.example.restservice;
 
+/**
+ * 클라이언트에게 전달할 응답 데이터의 구조를 정의하는 DTO 클래스
+ *  데이터만 담아두는 역할
+ * @param id
+ * @param content
+ */
 public record Greeting(long id, String content){}
